9e92f0026ef2a480ca13d3addb611924cea5ea09
Cizzuk <dropcat@cizzuk.net>
2025-12-22 10:49:31 +0900

    Add Notification

--- CaptureIntent.swift
+++ CaptureIntent.swift
@@ -7,12 +7,17 @@
 
 import AppIntents
 
+extension Notification.Name {
+    static let cameraControlDidActivate = Notification.Name("cameraControlDidActivate")
+}
+
 struct CaptureIntent: CameraCaptureIntent {
     static let title: LocalizedStringResource = "CaptureIntent"
     static var isDiscoverable: Bool = false
 
     @MainActor
     func perform() async throws -> some IntentResult {
+        NotificationCenter.default.post(name: .cameraControlDidActivate, object: nil)
         return .result()
     }
 }
