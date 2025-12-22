c612f5f930ebff42a737033eefb52203ae0be81f
Cizzuk <dropcat@cizzuk.net>
2025-12-22 09:41:22 +0900

    Create Project

---
+++ OpenFromCameraControllDemo/OpenFromCameraControllDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenFromCameraControllDemoApp.swift
+//  OpenFromCameraControllDemo
+//
+//  Created by Cizzuk on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OpenFromCameraControllDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
